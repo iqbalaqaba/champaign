31401813aeb2064a1858b0b2dc9a5284d585e618
Iqbal <iqbalwebsite@gmail.com>
2023-02-27 12:18:15 +0700

    Added local files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"], // add your preferred file extensions here
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9D22C2",
+        "primary-purple": "#7A1AD6",
+        "light-purple": "#F2E4FC",
+        "grayscale-10": "#fdfdfd",
+        "grayscale-20": "#ECF1F6",
+        "grayscale-30": "#E3E9ED",
+        "grayscale-40": "#D1D8DD",
+        "grayscale-50": "#BFC6CC",
+        "grayscale-60": "#9CA4AB",
+        "grayscale-70": "#78828A",
+        "grayscale-80": "#66707A",
+        "grayscale-90": "#434E58",
+        "grayscale-100": "#070717",
+      },
+    },
+  },
+  plugins: [],
+}
